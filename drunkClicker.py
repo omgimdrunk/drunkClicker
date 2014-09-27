b07c6c618cc511ee017a6dd95e42d4fb950246a1
omgimdrunk <omgimdrunk.net@gmail.com>
2014-09-26 23:44:06 -0500

    Update drunkClicker.py

--- drunkClicker.py
+++ drunkClicker.py
@@ -1,20 +1,34 @@
 '''
 READ THIS FIRST!
 
-This script makes some inital assumptions, 
+This script makes some initial assumptions, 
 
 1) You already have the Vaagur ancient and it is at MAX 
-	The timeing of the buffs is based off of this.
+	The timing of the buffs is based off of this.
 	If not; you can still use this script with out the buffs by just using [-c X,Y]
 
 2) You have all the available buffs from your adventures.
-	Not nessacery for it to work but I am just assuming.
+	Not necessary for it to work but I am just assuming.
 	
-3) This script may be CPU intensive to older systems. May cause lag.
+3) You are using Windows - this is a must, no way around it. 
+	to use: 
+	- install python 3.x ( I used 3.4 )
+	- install the win32api for python - http://sourceforge.net/projects/pywin32/files/pywin32/
+	- from cmd, cd to the location of drunkCLicker.py
+	- type python drunkClicker.py [-t x,y] to test where the mouse will move to on your screen.
+		eg: [python drunkCLicker.py -t 1700,600]
+		position this so that the mouse lands on top of the monster.
+	- Once mouse position is confirmed -c x,y or -bc x,y can be used to start the clicking.
+		eg: [python drunkClicker.py -c 1700,600] this will perform just mouse clicks.
+		eg: [python drunkCLicker.py -bc 1700,600] This will perform mouse clicks with DR Ceremony.
+
+4) This script may be CPU intensive to older systems. May cause lag.
 	As a fail safe, moving the mouse will end this script.
 
-4) If you experienace system unresponsiveness, 
+5) If you experience system unresponsiveness, 
 	you can adjust the User Defined Variables below.
+	
+
 '''
 
 from time import sleep
@@ -136,6 +150,7 @@ def darkRitload () :
 
 def buffStart () :
 	'''
+	THE DR CEREMONY
 	Finger Blast Buffs (1,2,3,4,5,7)
 	Dark Ritual combo (8,6,9)
 	Wait 15 minutes
@@ -150,11 +165,11 @@ def buffStart () :
 	while mvPOS != 0 :
 		fngrBlast ()
 		darkRit ()
-		print ('Dark Ritual Ceremony has started, will complete at ' + timesUP(15) )
+		print ('DR START : Dark Ritual Ceremony has started, will complete at ' + timesUP(15) )
 		if holdUp (drCD) == 0: #A return of 0 means the mouse has moved in MAIN__
 			return
 		darkRitload ()
-		print ('DR DONE : Dark Ritual Ceremony is complete, will begin at ' + timesUP(15) )
+		print ('DR DONE : Dark Ritual Ceremony is complete, next ceremony at ' + timesUP(15) )
 		#Repeat the none DR buffs 6 times, 2.5 min each *6 = 15min
 		for r in range(6) :
 			fngrBlast ()
